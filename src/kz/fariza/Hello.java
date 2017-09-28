4e5e9124a860993f2a3c5cf2cb4d8d500b50063f
Khalamkhan Fariza <khalamhan@mail.ru>
2017-09-29 01:33:15 +0600

    Екінші коммит

--- src/kz/fariza/Hello.java
+++ src/kz/fariza/Hello.java
@@ -3,5 +3,8 @@ package kz.fariza;
 public class Hello {
     public static void main (String[] args){
         System.out.println("Hello, Fariza!");
+        System.out.println("Бұл зертханалық жұмыстың мақсаты:");
+        System.out.println("GitHub-пен жұмыс жасап үйрену:");
+
     }
 }
