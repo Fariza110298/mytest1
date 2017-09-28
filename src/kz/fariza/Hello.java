edc4d58f2507f69556d4f20ed108f417126f348d
Khalamkhan Fariza <khalamhan@mail.ru>
2017-09-29 01:56:09 +0600

    Үшінші коммит

--- src/kz/fariza/Hello.java
+++ src/kz/fariza/Hello.java
@@ -5,6 +5,10 @@ public class Hello {
         System.out.println("Hello, Fariza!");
         System.out.println("Бұл зертханалық жұмыстың мақсаты:");
         System.out.println("GitHub-пен жұмыс жасап үйрену:");
+        int a=MyMath.kvadrat(5);
+        int b=MyMath.kub(5);
+        System.out.println("a="+a);
+        System.out.println("b="+b);
 
     }
 }
