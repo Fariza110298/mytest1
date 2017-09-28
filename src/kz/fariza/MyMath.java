bf9b8b69acab48d5ab1dc205da7da44185261b29
Khalamkhan Fariza <khalamhan@mail.ru>
2017-09-29 02:05:22 +0600

    experiment1 бұтагында Hello.java MyMath.java файлдарына өзгеріс жасалды.

--- src/kz/fariza/MyMath.java
+++ src/kz/fariza/MyMath.java
@@ -7,4 +7,18 @@ public class MyMath {
         return a*a*a;
 
     }
+    public static boolean isEven(int a){
+            return (a%2==0);
+    }
+    public static int max(int a, int b){
+        if (a>=b)
+            return a;
+        else return b;
+    }
+    public static int min(int a, int b){
+        if (a>=b)
+            return b;
+        else
+            return a;
+    }
 }
