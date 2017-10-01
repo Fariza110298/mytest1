2cf35bc0684d685dcabe340f148952b7a5a14ad0
Khalamkhan Fariza <khalamhan@mail.ru>
2017-10-02 05:39:00 +0600

    2 commit

--- src/kz/fariza/Helloo.java
+++ src/kz/fariza/Helloo.java
@@ -2,5 +2,8 @@ package kz.fariza;
 
 public class Helloo {
     public static void main(String[]args){
-        System.out.println("Hello, Fariza");}
+        System.out.println("Hello, Fariza");
+        System.out.println(" Fariza");
+        System.out.println("Khalamkhan");
+    }
 }
