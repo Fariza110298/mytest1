58accbf7b8077bad8094d12d4d63a8ab87b29fe1
Khalamkhan Fariza <khalamhan@mail.ru>
2017-10-02 01:23:52 +0600

    2-commit

--- src/kz/fariza/Hello1.java
+++ src/kz/fariza/Hello1.java
@@ -3,5 +3,7 @@ package kz.fariza;
 public class Hello1 {
     public static void main(String[]args){
         System.out.println("Hello, Fariza");
+        System.out.println("Khalamkhan");
+        System.out.println("Fariza");
     }
 }
