1a4cb15a389b37d9e2cbdcf25ab7914939adae44
Khalamkhan Fariza <khalamhan@mail.ru>
2017-10-02 07:09:34 +0600

    commit number 3

--- src/kz/fariza/fariza.java
+++ src/kz/fariza/fariza.java
@@ -5,6 +5,10 @@ public class fariza {
         System.out.println("Hello, Fariza");
         System.out.println(" Fariza");
         System.out.println("Khalamkhan");
+        int a=MyMath3.kvadrat(5);
+        int b=MyMath3.kub(5);
+        System.out.println("a="+a);
+        System.out.println("b="+b);
 
     }
 }
